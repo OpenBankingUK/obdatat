b5d6f98804e8bda1cbccf148d07e36849143bc64
Maciej Ziehlke <mac@dmio.co>
2017-08-18 11:54:41 +0100

    Initial script upload

---
+++ index.js
@@ -0,0 +1,73 @@
+const 
+  fs = require('fs'),
+  nJwt = require('njwt'),
+  qs = require('qs'),
+  request = require('axios'),
+  chalk = require('chalk');
+
+console.log(chalk.bold.green("Open Banking test tool to obtain Access Token"));
+console.log(chalk.bold.green("---------------------------------------------"));
+console.log();
+
+// Load Private Key and config from files
+const signingKey = fs.readFileSync(`${__dirname}/config/privatekey.pem`); // ES512
+const config = JSON.parse(fs.readFileSync(`${__dirname}/config/config.json`));
+
+const claims = {
+  iss: config.softwareStatementId,
+  sub: config.softwareStatementId,
+  scope: config.clientScopes,
+  aud: config.tokenUrl
+}
+
+const created_jwt = nJwt.create(claims, signingKey, 'RS256');
+created_jwt.setHeader('kid', config.keyId);
+
+console.log(chalk.bold.blue("Created JWT:"), created_jwt.compact());
+console.log();
+
+// Configure the request to obtain token
+const tokenRequestSpec = {
+  url: config.tokenUrl,
+  method: 'POST',
+  data: qs.stringify({
+    'client_assertion_type': 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+    'grant_type': 'client_credentials',
+    'client_id': config.softwareStatementId,
+    'client_assertion': created_jwt.compact(),
+    'scope': config.clientScopes
+  })
+}
+
+// Send request to get the token
+request(tokenRequestSpec)  
+  .then((response) => {
+    console.log(chalk.bold.blue("Token acquired:"), response.data.access_token);
+    console.log();
+
+    // Configure the request for test endpoint - list of TPPs
+    const tppRequestSpec = {
+      url: config.tppTestUrl,
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${response.data.access_token}`
+      }
+    }
+
+    return request(tppRequestSpec);
+
+  })
+  .then((response) => {
+    console.log(chalk.bold.blue("TPPs"));
+
+    // Test request to get the list of TPPs
+    response.data.Resources.forEach((tpp) => {
+      const org = tpp['urn:openbanking:organisation:1.0'];
+      console.log("-", org.OrganisationCommonName);
+    });
+
+  })
+  .catch((err) => {
+    // Display errors
+    console.log(chalk.bold.red('Error:'), err.response.data);
+  })
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Access Token Acquisition Tool
+
+## Installation
+- Clone this repository.
+- Update `config/privatekey.pem` with your key *from the DFI*.
+- Update `config/config.json` with the required data.
+- Run `npm i` from installation folder.
+
+## Usage
+Run `npm start` from installation folder.
