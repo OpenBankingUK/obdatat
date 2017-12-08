47e20d1d37239c971e6869dbb085758ee7d11cd6
Maciej Ziehlke <mac@dmio.co>
2017-12-08 12:46:16 +0000

    MATLS version of tool

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# Access Token Acquisition Tool
+# Access Token Acquisition Tool (MATLS)
+
+For non-MATLS (old) version of this tool check `non-matls` branch.
 
 ## Installation
-- Clone this repository.
-- Update `config/privatekey.pem` with your key *from the DFI*.
-- Update `config/config.json` with the required data.
-- Run `npm i` from installation folder.
+- Clone this repository
+- Create new Software Statement in the OB DFI
+- Generate 2 keypairs for the Signing and Transport certs
+- Make sure you follow the instructions for generating CSRs 
+- Update `config/*.key` and `config/*.pem` files with private keys generated with CSR and PEM files downloaded from DFI
+- Update `config/config.json` with the required data
+- Run `npm i` from installation folder
 
 ## Usage
-Run `npm start` from installation folder.
+- Install Node package dependencies running `npm i` via the command line
+- Run `npm start` from installation folder
 
 Copyright 2017 Open Banking Limited. All rights reserved.
 The software provided has no warranty, it is provided “as is”. It is your responsibility to validate the behavior of the routines and their accuracy using the source code provided, or to purchase support and warranties from commercial redistributors.
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const
-  fs = require('fs'),
-  nJwt = require('njwt'),
-  qs = require('qs'),
-  request = require('axios'),
-  chalk = require('chalk')
-  boxen = require('boxen');
+fs = require('fs'),
+nJwt = require('njwt'),
+qs = require('qs'),
+request = require('axios'),
+https = require('https'),
+chalk = require('chalk'),
+boxen = require('boxen');
 
 console.log(chalk.bold.green(boxen("Open Banking Directory Access Token Acquisition Tool", {
   margin: 1,
@@ -14,14 +15,25 @@ console.log(chalk.bold.green(boxen("Open Banking Directory Access Token Acquisit
 console.log();
 
 // Load Private Key and config from files
-const signingKey = fs.readFileSync(`${__dirname}/config/privatekey.pem`); // ES512
 const config = JSON.parse(fs.readFileSync(`${__dirname}/config/config.json`));
+const signingCert = fs.readFileSync(`${__dirname}/config/certSigning.pem`);
+const signingKey = fs.readFileSync(`${__dirname}/config/privateKeySigning.key`); // ES512
+const transportCert = fs.readFileSync(`${__dirname}/config/certTransport.pem`);
+const transportKey = fs.readFileSync(`${__dirname}/config/privateKeyTransport.key`); // ES512
+
+// Node doesn't support concatenated CAs in a single PEM
+// Read both files into the globalAgents file one at a time.
+const trustedCa = [
+  `${__dirname}/config/root.pem`,
+  `${__dirname}/config/issuingca.pem`,
+  `${__dirname}/config/signingca.pem`
+];
 
 const claims = {
-  iss: config.softwareStatementId,
-  sub: config.softwareStatementId,
-  scope: config.clientScopes,
-  aud: config.aud
+iss: config.softwareStatementId,
+sub: config.softwareStatementId,
+scope: config.clientScopes,
+aud: config.aud
 };
 
 const created_jwt = nJwt.create(claims, signingKey, 'RS256');
@@ -31,9 +43,24 @@ const compacted_jwt = created_jwt.compact();
 console.log(chalk.bold.blue("Created JWT:"), compacted_jwt);
 console.log();
 
+
+//Blank the CA list and load only the MA ones
+var httpsAgent = https.globalAgent;
+https.globalAgent.options.ca = [];
+for (const ca of trustedCa) {
+  https.globalAgent.options.ca.push(fs.readFileSync(ca));
+  httpsAgent = new https.Agent({
+      cert: transportCert,
+      key: transportKey,
+      ca: https.globalAgent.options.ca,
+      rejectUnauthorized: true
+  });
+}
+
 // Configure the request to obtain token
 const tokenRequestSpec = {
   url: config.tokenUrl,
+  httpsAgent: httpsAgent,
   method: 'POST',
   data: qs.stringify({
     'client_assertion_type': 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
@@ -45,41 +72,44 @@ const tokenRequestSpec = {
 };
 
 console.log(chalk.bold.blue("Requesting Access Token..."));
-console.log(tokenRequestSpec);
 console.log();
 
 const errorHandler = function(error) {
   console.log(chalk.red.bold(error));
   console.log(chalk.blue.bold("Response body:"));
   console.log(error.response.data);
+  console.log(error.response.status);
 };
 
 // Send request to get the token
 request(tokenRequestSpec)
-  .then((response) => {
-    console.log(chalk.bold.blue("Token acquired:"), response.data.access_token);
-    console.log();
+.then((response) => {
+  console.log(chalk.bold.blue("Token acquired:"), response.data.access_token);
+  console.log();
 
-    // Configure the request for test endpoint - list of TPPs
-    const tppRequestSpec = {
-      url: config.tppTestUrl,
-      method: "GET",
-      headers: {
-        "Authorization": `Bearer ${response.data.access_token}`
-      }
-    };
+  // Configure the request for test endpoint - list of TPPs
+  const tppRequestSpec = {
+    url: config.tppTestUrl,
+    httpsAgent: httpsAgent, 
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${response.data.access_token}`
+    }
+  };
 
-    return request(tppRequestSpec);
+  return request(tppRequestSpec);
 
-  })
-  .then((response) => {
-    console.log(chalk.bold.blue("TPPs"));
+})
+.then((response) => {
 
-    // Test request to get the list of TPPs
-    response.data.Resources.forEach((tpp) => {
-      const org = tpp['urn:openbanking:organisation:1.0'];
-      console.log("-", org.OrganisationCommonName);
-    });
+  // Test request to get the list of TPPs
+  response.data.Resources.forEach((tpp) => {
+    const org = tpp['urn:openbanking:organisation:1.0'];
+    const auth = tpp['urn:openbanking:competentauthorityclaims:1.0'];
+    const authorisation =tpp['urn:openbanking:accountservicepaymentserviceprovider:1.0'];
 
-  })
-  .catch(errorHandler);
+    console.log("-", org.OrganisationCommonName, "-", auth.Authorisations);
+  });
+
+})
+.catch(errorHandler);
